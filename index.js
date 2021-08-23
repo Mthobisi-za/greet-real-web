d394dee8e825d656e2875431ca90ed413fa997f6
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-23 11:48:52 +0200

    Remove route

--- index.js
+++ index.js
@@ -41,9 +41,11 @@ const PORT = process.env.PORT || 5000;
 ==method == get
 ----Route must serve the home page
 */
+/*
 app.get('/', (req,res)=>{
     res.render("index")
 })
+*/
 app.get("/", (req, res) => {
    dbLogic().getData(res);
   /*
