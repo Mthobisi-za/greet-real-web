5716ec4ed4c47e74c61b35cd0c33f320214fb57c
Mthobisi-za <mthobisimtho333@gmail.com>
2021-09-03 00:20:36 +0200

    travis

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const {Pool} = require("pg");
 // we are using a special test database for the tests
 const connectionString = process.env.DATABASE_URL;
 //{connectionString, ssl: {rejectUnauthorized: false}}
-//var obj = {user: "postgres",host: "localhost",database: "users",password: "mthobisi",port: 5432}
-var pool = new Pool({connectionString, ssl: {rejectUnauthorized: false}});
+var obj = {user: "postgres",host: "localhost",database: "users",password: "mthobisi",port: 5432}
+var pool;
+if(connectionString){
+  pool = new Pool({connectionString, ssl: {rejectUnauthorized: false}});
+} else{
+  pool = new Pool(obj)
+}
 //---require the database
 
 
